474370f6f5472f92e3ed85f70510ff5e24071d69
“JoseDavid” <joseaculex84@gmail.com>
2020-05-21 01:23:22 -0400

    Primer Commit

---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    descripcion: 'Marca como completado o pendiente la tarea'
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actuaiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+//Exportamos el objeto ARGV con sus comandos y funciones, para ser utilizado
+//en otros archivos js 
+module.exports = {
+    argv
+}
